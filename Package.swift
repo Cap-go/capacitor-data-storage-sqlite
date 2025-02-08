028007fca288f0947219d5ca93f4adbccc4a3b38
Martin Donadieu <martindonadieu@gmail.com>
2025-02-08 17:00:49 +0000

    fix: add SMP

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "CapgoCapacitorDataStorageSqlite",
+    platforms: [.iOS(.v14)],
+    products: [
+        .library(
+            name: "CapgoCapacitorDataStorageSqlite",
+            targets: ["CapgoCapacitorDataStorageSqlitePlugin"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0")
+        .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.15.3")
+
+    ],
+    targets: [
+        .target(
+            name: "CapgoCapacitorDataStorageSqlitePlugin",
+            dependencies: [
+                .product(name: "Capacitor", package: "capacitor-swift-pm"),
+                .product(name: "Cordova", package: "capacitor-swift-pm"),
+                .product(name: "SQLite", package: "SQLite.swift/SQLCipher")
+            ],
+            path: "ios/Sources/CapgoCapacitorDataStorageSqlitePlugin"),
+        .testTarget(
+            name: "CapgoCapacitorDataStorageSqlitePluginTests",
+            dependencies: ["CapgoCapacitorDataStorageSqlitePlugin"],
+            path: "ios/Tests/CapgoCapacitorDataStorageSqlitePluginTests")
+    ]
+)
