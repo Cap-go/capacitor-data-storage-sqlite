cc6112d0a14596ff010568f5379764b5dc8853de
Jordan Lorho <jordan.lorho@gmail.com>
2025-10-08 13:10:16 +0200

    chore(ios): migrate to SPM

--- Package.swift
+++ Package.swift
@@ -3,29 +3,33 @@ import PackageDescription
 
 let package = Package(
     name: "CapgoCapacitorDataStorageSqlite",
-    platforms: [.iOS(.v14)],
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
         .library(
             name: "CapgoCapacitorDataStorageSqlite",
-            targets: ["CapgoCapacitorDataStorageSqlitePlugin"])
+            targets: ["CapgoCapacitorDataStorageSqlite"]
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.4.3")
-        .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.15.3")
-
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.4.3"),
+        .package(url: "https://github.com/stoneburner/SQLCipher.git", from: "0.0.4")
     ],
     targets: [
         .target(
-            name: "CapgoCapacitorDataStorageSqlitePlugin",
+            name: "CapgoCapacitorDataStorageSqlite",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
-                .product(name: "SQLite", package: "SQLite.swift/SQLCipher")
+                .product(name: "SQLCipher", package: "SQLCipher")
             ],
-            path: "ios/Sources/CapgoCapacitorDataStorageSqlitePlugin"),
+            path: "ios/Sources/CapgoCapacitorDataStorageSqlite",
+        ),
         .testTarget(
-            name: "CapgoCapacitorDataStorageSqlitePluginTests",
-            dependencies: ["CapgoCapacitorDataStorageSqlitePlugin"],
-            path: "ios/Tests/CapgoCapacitorDataStorageSqlitePluginTests")
+            name: "CapgoCapacitorDataStorageSqliteTests",
+            dependencies: ["CapgoCapacitorDataStorageSqlite"],
+            path: "ios/Tests/CapgoCapacitorDataStorageSqliteTests",
+        )
     ]
 )
