002e446b51f50b0ce3983f77345750da26f09fec
Martin Donadieu <martindonadieu@gmail.com>
2025-10-23 06:19:20 +0100

    fix: remove dist

--- dist/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { CapgoCapacitorDataStorageSqlitePlugin } from "./definitions";
-declare const CapgoCapacitorDataStorageSqlite: CapgoCapacitorDataStorageSqlitePlugin;
-export * from "./definitions";
-export { CapgoCapacitorDataStorageSqlite };
--- dist/esm/index.js
+++
@@ -1,9 +0,0 @@
-import { registerPlugin } from "@capacitor/core";
-const CapgoCapacitorDataStorageSqlite = registerPlugin("CapgoCapacitorDataStorageSqlite", {
-    web: () => import("./web").then((m) => new m.CapgoCapacitorDataStorageSqliteWeb()),
-    electron: () => window.CapacitorCustomPlatform.plugins
-        .CapacitorDataStorageSqlite,
-});
-export * from "./definitions";
-export { CapgoCapacitorDataStorageSqlite };
-//# sourceMappingURL=index.js.map
