af1de2d0091b572f9311a89c01fca18202637bf6
Martin Donadieu <martindonadieu@gmail.com>
2025-10-29 18:57:27 +0000

    fix: version

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
-        .package(url: "https://github.com/zhuorantan/SQLiteCipher.git", from: "7.0.0")
+        .package(url: "https://github.com/zhuorantan/SQLiteCipher.git", from: "0.0.1")
     ],
     targets: [
         .target(
